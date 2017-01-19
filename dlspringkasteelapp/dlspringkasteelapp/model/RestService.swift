350e8662460eb92d5a581a46e0fdea08c5b9e773
simonjang <simon.jang.contact@gmail.com>
2017-01-19 09:08:41 +0100

    Alamofire voor REST calls

--- dlspringkasteelapp/dlspringkasteelapp/model/RestService.swift
+++ dlspringkasteelapp/dlspringkasteelapp/model/RestService.swift
@@ -2,12 +2,23 @@
 //  RestService.swift
 //  springkasteelapp
 
-// Code gebaseerd op https://devdactic.com/parse-json-with-swift/ voor REST calls
+// Code gebaseerd op 
+//      https://devdactic.com/parse-json-with-swift/
+//      https://github.com/Alamofire/Alamofire voorbeelden
+//      https://developer.apple.com/swift/blog/?id=37 laatste voorbeeld
 //  Deze klasse haalt de data op van de REST service
 // TODO implementeren asap maar eerst CocoaPods configureren
 
+import SwiftyJSON
+
 import Foundation
-class RestService {
+class RestService: NSObject {
+    
+    static let sharedInstance = RestService()
+    let baseUrl = "https://x.herokuapp.com/"
+    
+    // GET Request voor alle winkels (inclusief aantal springkastelen
+    
     static func getWinkels() -> [Int:Winkel]? {
         // TODO GET van REST-service
         return [:]
