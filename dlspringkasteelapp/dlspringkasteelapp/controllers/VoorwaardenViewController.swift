969fab8ed8ca4eca20dd5b48f17ad78aa12cae63
simonjang <simon.jang.contact@gmail.com>
2017-01-20 10:38:57 +0100

    SplitView issue opgelost

--- dlspringkasteelapp/dlspringkasteelapp/controllers/VoorwaardenViewController.swift
+++ dlspringkasteelapp/dlspringkasteelapp/controllers/VoorwaardenViewController.swift
@@ -8,6 +8,24 @@ import UIKit
 
 class VoorwaardenViewController: UIViewController {
 
+    
+    @IBAction func praktBtn() {
+       /* if let split = self.splitViewController {
+            let controllers = split.viewControllers
+            let detailViewController = controllers[controllers.count - 1] as? HuurDetailSplitViewController
+            detailViewController?.praktischValue = true
+            detailViewController?.viewDidLoad()
+        }*/
+    }
+    @IBAction func voorwaardenBtn() {
+        /*if let split = self.splitViewController {
+            let controllers = split.viewControllers
+            let detailViewController = controllers[controllers.count - 1] as? HuurDetailSplitViewController
+            detailViewController?.voorwaardenValue = true
+            detailViewController?.viewDidLoad()
+        }*/
+    }
+ 
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -18,26 +36,26 @@ class VoorwaardenViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if let identifier = segue.identifier {
             switch(identifier) {
                 case "showPraktisch":
-                    let destination = segue.destination as? HuurDetailSplitViewController
-                    destination?.praktischValue = true
-                case "showVoorwaarden":
-                    if let split = self.splitViewController {
-                        let controllers = split.viewControllers
-                        let detailViewController = controllers[controllers.count - 1] as? HuurDetailSplitViewController
-                        detailViewController?.praktischValue = false
+                    if let destination = segue.destination as? HuurDetailSplitViewController {
+                        destination.praktischValue = true
                     }
-                    let destination = segue.destination as? HuurDetailSplitViewController
-                    destination?.voorwaardenValue = true
+                case "showVoorwaarden":
+                    if let destination = segue.destination as? HuurDetailSplitViewController {
+                       destination.voorwaardenValue = true
+                    }
                 default:
                 break
             }
         }
     }
- 
-
 }
+
+
+
+// Backup code voor segue oplossing
+// Werkt dan op iphone maar niet op ipad
