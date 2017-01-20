60f017e6f4151061b0f19d7e811e3076cb92c1f2
simonjang <simon.jang.contact@gmail.com>
2017-01-20 15:08:40 +0100

    Kleine aanpassing in navigatieherder

--- dlspringkasteelapp/dlspringkasteelapp/controllers/BevestigingViewController.swift
+++ dlspringkasteelapp/dlspringkasteelapp/controllers/BevestigingViewController.swift
@@ -1,10 +1,7 @@
 //
 //  BevestigingViewController.swift
 //  dlspringkasteelapp
-//
-//  Created by Simon Jang on 20/01/17.
-//  Copyright © 2017 Simon Jang. All rights reserved.
-//
+
 
 import UIKit
 
@@ -26,15 +23,5 @@ class BevestigingViewController: UIViewController {
             controller.popToRootViewController(animated: true)
         }
     }
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
